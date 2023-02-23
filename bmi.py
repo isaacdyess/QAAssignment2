f5e8dd05e94db42ae26430dab55a7631c4ce9946
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-02-23 13:56:27 -0600

    Split operations into functions.

--- bmi.py
+++ bmi.py
@@ -1,20 +1,34 @@
 def calculate_bmi(feet, inches, pounds):
-    kilos = pounds * 0.45
-    inches += (feet * 12.0)
-    meters = inches * 0.025
+    kilos = conv_lbs_kilos(pounds)
+    inches += conv_ft_in(feet)
+    meters = conv_in_m(inches)
     bmi = kilos / (meters ** 2.0) 
     return bmi
+
+def conv_lbs_kilos(lbs):
+    return lbs * 0.45
+def conv_ft_in(ft):
+    return feet * 12.0
+def conv_in_m(inches):
+    return inches* 0.025
+
+
+def generate_output(bmi):
+    output = str(bmi) + "; "
+    if user_bmi < 18.5:
+        output += "Underweight."
+    elif user_bmi < 24.9:
+        output += "Normal weight."
+    elif user_bmi < 29.9:
+        output += "Overweight."
+    else:
+        output += "Obese."
+
+    return output
 
 feet = float(input("Enter your height in feet: "))
 inches = float(input("Enter your remaining height in inches: "))
 pounds = float(input("Enter your weight in pounds: "))
 
 user_bmi = calculate_bmi(feet, inches, pounds)
-if user_bmi < 18.5:
-    print(str(user_bmi) + "; Underweight.")
-elif user_bmi < 24.9:
-    print(str(user_bmi) + "; Normal weight.")
-elif user_bmi < 29.9:
-    print(str(user_bmi) + "; Overweight.")
-else:
-    print(str(user_bmi) + "; Obese.")
+print(generate_output(user_bmi))
