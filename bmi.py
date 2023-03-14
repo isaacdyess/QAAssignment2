b4dd546f5786fea1ddfa24383fc4e6778cb2378a
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-03-14 16:07:49 -0500

    Fixed issues with pytest failing due to stdin.

--- bmi.py
+++ bmi.py
@@ -1,34 +1,42 @@
+def main():
+    feet, inches, pounds = user_input()
+    user_bmi = calculate_bmi(feet, inches, pounds)
+    print(output(user_bmi))
+
+def user_input():
+    feet = float(input("Enter your height in feet: "))
+    inches = float(input("Enter your remaining height in inches: "))
+    pounds = float(input("Enter your weight in pounds: "))
+
+    return feet, inches, pounds
+
 def calculate_bmi(feet, inches, pounds):
     kilos = conv_lbs_kilos(pounds)
     inches += conv_ft_in(feet)
     meters = conv_in_m(inches)
     bmi = kilos / (meters ** 2.0) 
-    return bmi
+    return round(bmi, 2)
 
 def conv_lbs_kilos(lbs):
-    return lbs * 0.45
+    return round(lbs * 0.45, 2)
 def conv_ft_in(ft):
-    return feet * 12.0
+    return round(ft * 12.0, 2)
 def conv_in_m(inches):
-    return inches* 0.025
+    return round(inches * 0.025, 2)
 
 
-def generate_output(bmi):
-    output = str(bmi) + "; "
+def output(user_bmi):
+    output = "{:.2f}".format(user_bmi) + "; "
     if user_bmi < 18.5:
         output += "Underweight."
-    elif user_bmi < 24.9:
+    elif user_bmi < 25.0:
         output += "Normal weight."
-    elif user_bmi < 29.9:
+    elif user_bmi < 30.0:
         output += "Overweight."
     else:
         output += "Obese."
 
     return output
 
-feet = float(input("Enter your height in feet: "))
-inches = float(input("Enter your remaining height in inches: "))
-pounds = float(input("Enter your weight in pounds: "))
-
-user_bmi = calculate_bmi(feet, inches, pounds)
-print(generate_output(user_bmi))
+if __name__=="__main__":
+    main()
--- test_bmi.py
+++ test_bmi.py
@@ -1,4 +1,22 @@
+import pytest
 import bmi
 
-def test_bmi():
-    #TODO: Add test for bmi function
+# Boundary for Underweight
+@pytest.mark.parametrize("input,expected", [(0.00, "0.00; Underweight."), (18.49, "18.49; Underweight."), (18.50, "18.50; Normal weight.")])
+def test_output_underweight(input, expected):
+    assert bmi.output(input) == expected
+
+# Boundary for Normal weight
+@pytest.mark.parametrize("input,expected", [(18.49, "18.49; Underweight."), (18.50, "18.50; Normal weight."), (24.90, "24.90; Normal weight."), (25.00, "25.00; Overweight.")])
+def test_output_normal(input, expected):
+    assert bmi.output(input) == expected
+
+# Boundary for Overweight
+@pytest.mark.parametrize("input,expected", [(24.99, "24.99; Normal weight."), (25.00, "25.00; Overweight."), (29.99, "29.99; Overweight."), (30.00, "30.00; Obese.")])
+def test_output_over(input, expected):
+    assert bmi.output(input) == expected
+
+# Boundary for Obese
+@pytest.mark.parametrize("input,expected", [(29.99, "29.99; Overweight."), (30.00, "30.00; Obese.")])
+def test_output_obese(input, expected):
+    assert bmi.output(input) == expected
