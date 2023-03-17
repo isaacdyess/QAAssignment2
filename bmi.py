c0d5744b68b01c6200e3ff64aa2956c150a56939
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-03-17 16:07:48 -0500

    Finalized bmi program and tests.

--- bmi.py
+++ bmi.py
@@ -1,5 +1,6 @@
 def main():
-    feet, inches, pounds = user_input()
+    uservalues = user_input()
+    feet, inches, pounds = uservalues[0], uservalues[1], uservalues[2]
     user_bmi = calculate_bmi(feet, inches, pounds)
     print(output(user_bmi))
 
@@ -8,26 +9,23 @@ def user_input():
     inches = float(input("Enter your remaining height in inches: "))
     pounds = float(input("Enter your weight in pounds: "))
 
-    return feet, inches, pounds
+    if ((feet + inches) <= 0.00) or (pounds < 0.00):
+        raise ValueError("User input must be positive values.")
+
+    res = [feet, inches, pounds]
+    return res
 
 def calculate_bmi(feet, inches, pounds):
-    kilos = conv_lbs_kilos(pounds)
-    inches += conv_ft_in(feet)
-    meters = conv_in_m(inches)
-    bmi = kilos / (meters ** 2.0) 
+    weight = pounds * 0.45
+    height = ((feet * 12.0) + inches) * 0.025
+    if (weight < 0.00) or (height <= 0.00):
+        raise ValueError("User inputs must be positive values.")
+    bmi = weight / (height ** 2.0) 
     return round(bmi, 2)
-
-def conv_lbs_kilos(lbs):
-    return round(lbs * 0.45, 2)
-def conv_ft_in(ft):
-    return round(ft * 12.0, 2)
-def conv_in_m(inches):
-    return round(inches * 0.025, 2)
-
 
 def output(user_bmi):
     output = "{:.2f}".format(user_bmi) + "; "
-    if user_bmi < 18.5:
+    if user_bmi < 18.6:
         output += "Underweight."
     elif user_bmi < 25.0:
         output += "Normal weight."
