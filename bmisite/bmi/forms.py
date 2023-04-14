5ab7b62fb667b9fa16d56eec58b162162f003d65
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-04-13 19:18:00 -0500

    Finalized application.

--- bmisite/bmi/forms.py
+++ bmisite/bmi/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class BMIForm(forms.Form):
-    feet = forms.FloatField(label="Enter your height in feet")
+    feet = forms.FloatField(label="Enter your height in feet", min_value=0.01)
     inches = forms.FloatField(label="Enter your remaining height in inches")
-    pounds = forms.FloatField(label="Enter your weight in pounds")
+    pounds = forms.FloatField(label="Enter your weight in pounds", min_value=0.00)
